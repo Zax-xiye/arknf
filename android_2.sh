7cea4068f8e6c85884f8ecda83677e33bcbb8682
Zax-xiye <62531384+Zax-xiye@users.noreply.github.com>
2021-11-14 14:29:40 +0800

    Update android_2.sh

--- android_2.sh
+++ android_2.sh
@@ -1,10 +1,8 @@
 apt update
-apt install python
 apt install mitmproxy
 apt install git
 git clone https://github.com/Zax-xiye/arknf.git
 cd arknf
-sleep1
 touch ark_data.json
 echo 'cd arknf && mitmweb -p 8008 -s ark.py' > ~/.bashrc
 mitmweb -p 8008 -s ark.py
