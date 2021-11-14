1329310b6883eeffdfc1adc824ad17ffb14c7db4
Zax-xiye <62531384+Zax-xiye@users.noreply.github.com>
2021-11-14 12:54:02 +0800

    Update click_to_run.bat

--- click_to_run.bat
+++ click_to_run.bat
@@ -1,2 +1,1 @@
-powershell (new-object System.Net.WebClient).DownloadFile('https://gitee.com/jxr2006/ark-fatigue/raw/main/arknights.py','%~dp0arknights.py')
-mitmweb -p 8008 -s %~dp0arknights.py
+mitmweb -p 8008 -s ark.py
